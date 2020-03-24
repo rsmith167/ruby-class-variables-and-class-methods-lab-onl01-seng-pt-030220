56bbdbaa5414fba5e0167b9f513f5db90fbf6333
Rwanda S <rwanda.ace@gmail.com>
2020-03-24 16:36:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,17 +8,17 @@ class Song
   
   attr_accessor :name, :artist, :genre
   
-  def initialize(name, artists, genre)
+  def initialize(:name, :artists, :genre)
     @@song_instances +=1
     @@artists << :artist
     @@genres << :genre
-    if  @@genre_count[genre].length  >= 0
-      @@genre_count[genre] = @@genre_count[genre] + 1
+    if  @@genre_count[:genre].length  >= 0
+      @@genre_count[:genre] = @@genre_count[:genre] + 1
     else
-      @@genre_count[genre] = 1
+      @@genre_count[:genre] = 1
     end
-    if @@artist_count[name] >= 0
-      @@artist_count[name] = @@artist_count[name] + 1
+    if @@artist_count[:name] >= 0
+      @@artist_count[name] = @@artist_count[:name] + 1
     else
       @@artist_count[name] = 1
     end
